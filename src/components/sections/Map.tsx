85ac3274066ab7460a6fdfc02cb13e9021bcc74e
Kezia <roselinakeziarijadi@gmail.com>
2024-11-17 16:36:51 -0800

    render markers + sheets

--- src/components/sections/Map.tsx
+++ src/components/sections/Map.tsx
@@ -211,7 +211,10 @@ function VancMap() {
                   className="w-full h-full object-cover filter grayscale brightness-60"
                 />
               </div>
-              <SheetTitle className="text-white text-2xl font-serif">{story.name}</SheetTitle>
+              <div className="flex flex-row gap-[0.5rem]">
+                <SheetTitle className="text-white text-2xl font-serif">{story.name}</SheetTitle>
+                <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="#ffffff" viewBox="0 0 256 256" className="hover:underline"><path d="M200,64V168a8,8,0,0,1-16,0V83.31L69.66,197.66a8,8,0,0,1-11.32-11.32L172.69,72H88a8,8,0,0,1,0-16H192A8,8,0,0,1,200,64Z"></path></svg>
+              </div>
               <SheetDescription>
                 {story.story}
               </SheetDescription>
