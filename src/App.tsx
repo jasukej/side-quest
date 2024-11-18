7c3eb3ccc8d5636540cf1e39440d803e46ad583b
karenag <karenag@student.ubc.ca>
2024-11-17 17:32:30 -0800

    updated the text for marker

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ function App() {
       <APIProvider apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY || ""}>
         <div className="px-[3rem] py-[2rem]">
           <div className="text-4xl font-serif italic">Stories</div>
-          <div className="text-sm text-neutral-400">Click on a marker to learn more.</div>
+          <div className="text-sm text-neutral-400">Click on a microphone to hear their stories! Click on a landmark house to support.</div>
         </div>
         <VancMap />
       </APIProvider>
